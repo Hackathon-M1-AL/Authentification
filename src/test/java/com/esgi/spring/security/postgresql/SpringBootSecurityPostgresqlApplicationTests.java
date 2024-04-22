116c1b3049fc2388f1fb6087ab90ab6c63376faa
Matthieu Flce <flacelierematthieu@gmail.com>
2024-04-22 15:34:14 +0200

    fix: test

--- src/test/java/com/esgi/spring/security/postgresql/SpringBootSecurityPostgresqlApplicationTests.java
+++ src/test/java/com/esgi/spring/security/postgresql/SpringBootSecurityPostgresqlApplicationTests.java
@@ -2,8 +2,10 @@ package com.esgi.spring.security.postgresql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class SpringBootSecurityPostgresqlApplicationTests {
 
   @Test
