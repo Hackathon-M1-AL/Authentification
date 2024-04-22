70777ea89aaeb9b05782043c92f7e030f05121ee
Dmitri CHINE <dmitri.chine@gmail.com>
2024-04-22 12:24:07 +0200

    Init Roles when launching app

---
+++ src/main/java/com/bezkoder/spring/security/postgresql/config/RoleInitializer.java
@@ -0,0 +1,31 @@
+package com.bezkoder.spring.security.postgresql.config;
+
+import com.bezkoder.spring.security.postgresql.models.ERole;
+import com.bezkoder.spring.security.postgresql.models.Role;
+import com.bezkoder.spring.security.postgresql.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configuration
+public class RoleInitializer {
+
+    @Bean
+    CommandLineRunner initRoles(RoleRepository roleRepository) {
+        return args -> {
+            createRoleIfNotFound(roleRepository, ERole.ROLE_USER);
+            createRoleIfNotFound(roleRepository, ERole.ROLE_MODERATOR);
+            createRoleIfNotFound(roleRepository, ERole.ROLE_ADMIN);
+        };
+    }
+
+    @Transactional
+    void createRoleIfNotFound(RoleRepository roleRepository, ERole name) {
+        if (roleRepository.findByName(name).isEmpty()) {
+            Role role = new Role();
+            role.setName(name);
+            roleRepository.save(role);
+        }
+    }
+}
