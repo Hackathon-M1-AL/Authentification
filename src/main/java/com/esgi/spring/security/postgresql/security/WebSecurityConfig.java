e1330209f4d4d44775d8149a6752bc8a098b0853
Aurélien <aurelienpenotperbet@gmail.com>
2024-04-22 16:48:28 +0200

    feat add: swagger

--- src/main/java/com/esgi/spring/security/postgresql/security/WebSecurityConfig.java
+++ src/main/java/com/esgi/spring/security/postgresql/security/WebSecurityConfig.java
@@ -88,6 +88,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .authorizeHttpRequests(auth -> 
           auth.requestMatchers("/api/auth/**").permitAll()
               .requestMatchers("/api/test/**").permitAll()
+              .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
               .anyRequest().authenticated()
         );
     
