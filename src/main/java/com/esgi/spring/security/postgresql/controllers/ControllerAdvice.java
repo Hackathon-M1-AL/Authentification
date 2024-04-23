4a4ba14d9268cdb58e0098b759c12c9b29fb427d
Dmitri CHINE <dmitri.chine@gmail.com>
2024-04-23 14:29:25 +0200

    New path end-point

--- src/main/java/com/esgi/spring/security/postgresql/controllers/ControllerAdvice.java
+++ src/main/java/com/esgi/spring/security/postgresql/controllers/ControllerAdvice.java
@@ -1,7 +1,8 @@
 package com.esgi.spring.security.postgresql.controllers;
 
+import com.esgi.spring.security.postgresql.payload.response.JwtErrorDTO;
 import com.esgi.spring.security.postgresql.payload.response.MessageResponse;
-import com.esgi.spring.security.postgresql.utils.exception.CustomExpiredJwtTokenException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +11,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler(CustomExpiredJwtTokenException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<?> handleException(Exception e) {
+        JwtErrorDTO jwtResponse = new JwtErrorDTO("/error",
+                                                  HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                                                  e.getMessage(),
+                                                  HttpStatus.BAD_REQUEST.value());
+
+
         return ResponseEntity.badRequest()
-                             .body(new MessageResponse(e.getMessage()));
+                             .body(mapper.convertValue(jwtResponse,
+                                                       JwtErrorDTO.class));
     }
 }
